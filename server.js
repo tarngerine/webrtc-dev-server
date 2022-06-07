7de464eba7583e9e046b04daef799be3360db947
Julius Tarng <1629901+tarngerine@users.noreply.github.com>
2022-06-07 11:22:26 -0500

    cleanup, try catch

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express"),
   http = require("http"),
   app = express(),
   server = http.createServer(app),
-  path = require("path"),
   WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ server, path: "/ws" });
@@ -14,34 +13,39 @@ wss.on("connection", function connection(ws) {
     console.log("received: %s", message);
 
     // When a new user joins, pick another user to sync data over
-    const payload = JSON.parse(message);
-    console.log(payload);
-    if (payload.event === "join") {
-      // store client by client generated ID
-      clients[payload.id] = ws;
+    try {
+      const payload = JSON.parse(message);
+      console.log("message parsed", JSON.stringify(payload, null, 2));
 
-      for (var i = 0; i < wss.clients.length; i++) {
-        const client = wss.clients[i];
+      // interface Payload {
+      //   event: "join";
+      //   id: string;
+      // }
+      if (payload.event === "join") {
+        // store client by client generated ID
+        clients[payload.id] = ws;
+
+        // Cleanup handler
+        ws.on("close", () => {
+          delete clients[payload.id];
+        });
+      }
+
+      wss.clients.forEach((client) => {
+        // dont send payload back to the same client
         if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(message);
-          console.log("SYNCER selected", client);
-          break;
+          client.send(JSON.stringify(payload));
         }
-      }
+      });
+    } catch (e) {
+      console.log("message not valid json", e);
+      ws.send(
+        JSON.stringify({
+          event: "error",
+          message: "message not valid json",
+        })
+      );
     }
-
-    // The user that receives 'join' payload will sync back data to new user
-    if (payload.event === "sync") {
-      console.log("SYNC", payload);
-      clients[payload.id].send(message);
-    }
-
-    wss.clients.forEach(function each(client) {
-      // dont send to same client
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
   });
 });
 
